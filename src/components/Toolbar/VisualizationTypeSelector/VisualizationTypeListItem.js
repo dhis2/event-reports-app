1f3551a3a9cc25939ab338cf14a1492bb9c6917a
Edoardo Sabadelli <edoardo@dhis2.org>
2021-10-22 10:45:47 +0200

    fix: use cx for combining class names

--- src/components/Toolbar/VisualizationTypeSelector/VisualizationTypeListItem.js
+++ src/components/Toolbar/VisualizationTypeSelector/VisualizationTypeListItem.js
@@ -1,3 +1,4 @@
+import cx from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
 import ListItemIcon from './ListItemIcon'
@@ -10,34 +11,28 @@ const VisualizationTypeListItem = ({
     disabled,
     isSelected,
     onClick,
-}) => {
-    const classNames = [classes.listItem]
-
-    if (isSelected) {
-        classNames.push(classes.listItemActive)
-    }
-
-    if (disabled) {
-        classNames.push(classes.listItemDisabled)
-    }
-
-    return (
-        <div className={classNames.join(' ')} onClick={onClick}>
-            <div className={classes.listItemIcon}>
-                {
-                    <ListItemIcon
-                        iconType={iconType}
-                        style={{ width: 48, height: 48 }}
-                    />
-                }
-            </div>
-            <div className={classes.listItemText}>
-                <p className={classes.listItemName}>{label}</p>
-                <p className={classes.listItemDescription}>{description}</p>
-            </div>
+}) => (
+    <div
+        className={cx(classes.listItem, {
+            [classes.listItemActive]: isSelected,
+            [classes.listItemDisabled]: disabled,
+        })}
+        onClick={onClick}
+    >
+        <div className={classes.listItemIcon}>
+            {
+                <ListItemIcon
+                    iconType={iconType}
+                    style={{ width: 48, height: 48 }}
+                />
+            }
         </div>
-    )
-}
+        <div className={classes.listItemText}>
+            <p className={classes.listItemName}>{label}</p>
+            <p className={classes.listItemDescription}>{description}</p>
+        </div>
+    </div>
+)
 
 VisualizationTypeListItem.propTypes = {
     description: PropTypes.string,
