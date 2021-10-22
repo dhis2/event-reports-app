7c01092347386084f60959e16c20317e96903d0c
Martin Ohlson <martin@moid.se>
2021-10-22 11:34:36 +0200

    fix: add update button and proper design for the toolbar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,3 +1,4 @@
+import cx from 'classnames'
 import React from 'react'
 import classes from '../App.module.css'
 import { default as MenuBar } from './MenuBar'
@@ -6,11 +7,16 @@ const apiObjectName = 'eventReport' // TODO move to App?
 
 export const Toolbar = () => {
     return (
-        <div className={`${classes.sectionToolbar} ${classes.flexCt}`}>
+        <div className={cx(classes.sectionToolbar, classes.flexCt)}>
             <div className={classes.toolbarType}>
                 <span style={{ color: 'red' }}>{'vis type selector'}</span>
             </div>
-            <MenuBar apiObjectName={apiObjectName} dataTest={'app-menubar'} />
+            <div className={cx(classes.toolbarMenubar, classes.flexGrow1)}>
+                <MenuBar
+                    apiObjectName={apiObjectName}
+                    dataTest={'app-menubar'}
+                />
+            </div>
         </div>
     )
 }
