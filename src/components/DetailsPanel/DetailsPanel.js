dbddc5400d9c9b034d10d932f7613e5ec26706bb
Edoardo Sabadelli <edoardo@dhis2.org>
2021-11-02 15:12:42 +0100

    feat: use AboutVisualizationUnit in right sidebar

--- src/components/DetailsPanel/DetailsPanel.js
+++ src/components/DetailsPanel/DetailsPanel.js
@@ -1,8 +1,22 @@
+import { AboutVisualizationUnit } from '@dhis2/analytics'
+import PropTypes from 'prop-types'
 import React from 'react'
+import { connect } from 'react-redux'
+import { sGetCurrent } from '../../reducers/current'
 import classes from './styles/DetailsPanel.module.css'
 
-export const DetailsPanel = () => (
+export const DetailsPanel = ({ visualization }) => (
     <div className={classes.panel}>
-        <span style={{ color: 'red' }}>Details and interpretations panel</span>
+        <AboutVisualizationUnit visualization={visualization} />
     </div>
 )
+
+DetailsPanel.propTypes = {
+    visualization: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+    visualization: sGetCurrent(state),
+})
+
+export default connect(mapStateToProps)(DetailsPanel)
