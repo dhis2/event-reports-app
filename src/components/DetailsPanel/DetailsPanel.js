a4224d762215ca7f8f39a8e84796c66a16d24a5e
Edoardo Sabadelli <edoardo@dhis2.org>
2021-11-05 09:37:23 +0100

    refactor: pass AO type and id instead of vis/interpretations object
    The units take care of fetching the data they need and in this way the
    app does not have to refetch the whole AO just to update the state of
    the subscribe and like button/icon.

--- src/components/DetailsPanel/DetailsPanel.js
+++ src/components/DetailsPanel/DetailsPanel.js
@@ -2,20 +2,35 @@ import { AboutVisualizationUnit } from '@dhis2/analytics'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { connect } from 'react-redux'
+import history from '../../modules/history'
 import { sGetCurrent } from '../../reducers/current'
+import { sGetUser } from '../../reducers/user'
+import { InterpretationsUnit } from './InterpretationsUnit/InterpretationsUnit'
 import classes from './styles/DetailsPanel.module.css'
 
-export const DetailsPanel = ({ visualization }) => (
+export const DetailsPanel = ({ currentUser, visualization }) => (
     <div className={classes.panel}>
-        <AboutVisualizationUnit visualization={visualization} />
+        <AboutVisualizationUnit type="eventReports" id={visualization.id} />
+        <InterpretationsUnit
+            type="eventReports"
+            id={visualization.id}
+            currentUser={currentUser}
+            onInterpretationClick={interpretationId =>
+                history.push(
+                    `/${visualization.id}?interpretationId=${interpretationId}`
+                )
+            }
+        />
     </div>
 )
 
 DetailsPanel.propTypes = {
+    currentUser: PropTypes.object.isRequired,
     visualization: PropTypes.object.isRequired,
 }
 
 const mapStateToProps = state => ({
+    currentUser: sGetUser(state),
     visualization: sGetCurrent(state),
 })
 
