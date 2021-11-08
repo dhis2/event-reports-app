c9da717a5bf33be58c37256f087e64fb7821fddd
Hendrik de Graaf <hendrik@dhis2.org>
2021-11-08 13:51:58 +0100

    chore: simplify interpretation like and unlike callbacks

--- src/components/DetailsPanel/InterpretationsUnit/Interpretation.js
+++ src/components/DetailsPanel/InterpretationsUnit/Interpretation.js
@@ -1,23 +1,13 @@
-import { useDataMutation } from '@dhis2/app-runtime'
+import { useDataEngine } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
 import { Parser as RichTextParser } from '@dhis2/d2-ui-rich-text'
 import { Button, IconReply16, IconThumbUp16, colors } from '@dhis2/ui'
 import cx from 'classnames'
 import moment from 'moment'
 import PropTypes from 'prop-types'
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Avatar } from './Avatar'
 import classes from './styles/Interpretation.module.css'
-
-const getLikeMutation = id => ({
-    resource: `interpretations/${id}/like`,
-    type: 'create',
-})
-
-const getUnlikeMutation = id => ({
-    resource: `interpretations/${id}/like`,
-    type: 'delete',
-})
 
 export const Interpretation = ({
     interpretation,
@@ -25,34 +15,19 @@ export const Interpretation = ({
     onClick,
     onLikeToggle,
 }) => {
-    const [isLikedByCurrentUser, setIsLikedByCurrentUser] = useState(false)
-
-    const likeInterpretationMutation = useMemo(
-        () => getLikeMutation(interpretation.id),
-        []
-    )
-
-    const unlikeInterpretationMutation = useMemo(
-        () => getUnlikeMutation(interpretation.id),
-        []
-    )
-
-    const onMutationComplete = res => {
+    const engine = useDataEngine()
+    const resource = `interpretations/${interpretation.id}/like`
+    const onComplete = res => {
+        console.log('onComplete', res)
         if (res.status === 'OK') {
             onLikeToggle()
         }
     }
-
-    const [likeInterpretation] = useDataMutation(likeInterpretationMutation, {
-        onComplete: onMutationComplete,
-    })
-
-    const [unlikeInterpretation] = useDataMutation(
-        unlikeInterpretationMutation,
-        {
-            onComplete: onMutationComplete,
-        }
-    )
+    const likeInterpretation = () =>
+        engine.mutate({ resource, onComplete, type: 'create' })
+    const unlikeInterpretation = () =>
+        engine.mutate({ resource, onComplete, type: 'delete' })
+    const [isLikedByCurrentUser, setIsLikedByCurrentUser] = useState(false)
 
     const onLikeClick = isLiked => e => {
         e.stopPropagation()
