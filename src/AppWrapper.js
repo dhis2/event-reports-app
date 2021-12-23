90f0382aaf27ab4a23d64389502ff1eaaad97723
Edoardo Sabadelli <edoardo@dhis2.org>
2021-12-23 13:45:26 +0100

    refactor: replace D2Shim, UserSettingsProvider with CurrentUserProvider
    CurrentUserProvider returns both user and user settings data.
    D2Shim is not needed anymore as we don't need to pass d2 to FileMenu, we
    pass currentUser from the provider instead, which is used in
    OpenFileMenu (d2.currentUser was used before).
    The local UserSettingsProvider became redundant since the settings info
    is returned by CurrentUserProvider in analytics.

--- src/AppWrapper.js
+++ src/AppWrapper.js
@@ -1,13 +1,12 @@
-import { apiFetchOrganisationUnitLevels } from '@dhis2/analytics'
+import {
+    apiFetchOrganisationUnitLevels,
+    CurrentUserProvider,
+} from '@dhis2/analytics'
 import { useDataEngine } from '@dhis2/app-runtime'
-import { D2Shim } from '@dhis2/app-runtime-adapter-d2'
 import React, { useState, useEffect, useCallback } from 'react'
 import { Provider as ReduxProvider } from 'react-redux'
 import thunk from 'redux-thunk'
 import App from './components/App.js'
-import UserSettingsProvider, {
-    UserSettingsCtx,
-} from './components/UserSettingsProvider.js'
 import configureStore from './configureStore.js'
 import metadataMiddleware from './middleware/metadata.js'
 import history from './modules/history.js'
@@ -42,42 +41,18 @@ const AppWrapper = () => {
         doFetch()
     }, [])
 
-    const d2Config = {
-        schemas: [],
-    }
-
     return (
         <ReduxProvider store={store}>
-            <UserSettingsProvider>
-                <UserSettingsCtx.Consumer>
-                    {({ userSettings }) => {
-                        return userSettings?.uiLocale ? (
-                            <D2Shim
-                                d2Config={d2Config}
-                                i18nRoot="./i18n_old"
-                                locale={userSettings.uiLocale}
-                            >
-                                {({ d2 }) => {
-                                    if (!d2) {
-                                        // TODO: Handle errors in d2 initialization
-                                        return null
-                                    } else {
-                                        return (
-                                            <App
-                                                initialLocation={
-                                                    history.location
-                                                }
-                                                ouLevels={ouLevels} // TODO: Unused by App.js?
-                                                userSettings={userSettings}
-                                            />
-                                        )
-                                    }
-                                }}
-                            </D2Shim>
-                        ) : null
-                    }}
-                </UserSettingsCtx.Consumer>
-            </UserSettingsProvider>
+            <CurrentUserProvider>
+                {({ userSettings }) =>
+                    userSettings?.uiLocale ? (
+                        <App
+                            initialLocation={history.location}
+                            ouLevels={ouLevels} // TODO: Unused by App.js?
+                        />
+                    ) : null
+                }
+            </CurrentUserProvider>
         </ReduxProvider>
     )
 }
