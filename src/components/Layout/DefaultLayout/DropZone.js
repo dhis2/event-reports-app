92c151b87154413902df2379ca41367d4f769bd8
Jen Jones Arnesen <jennifer@dhis2.org>
2022-02-23 07:57:17 +0100

    fix: drop zone under the items

--- src/components/Layout/DefaultLayout/DropZone.js
+++ src/components/Layout/DefaultLayout/DropZone.js
@@ -9,15 +9,27 @@ const DropZone = ({ position, axisId, firstElementId }) => {
     const { isOver, setNodeRef, active } = useDroppable({
         id: getDropzoneId(axisId, position),
     })
-    const draggingOverFirstPosDropZone =
-        firstElementId === active?.id ? false : isOver
+
+    let draggingOver
+    if (position === FIRST) {
+        draggingOver = firstElementId === active?.id ? false : isOver
+    } else {
+        draggingOver = isOver
+    }
+
+    let isEmpty
+    if (position === LAST) {
+        isEmpty = false
+    } else {
+        isEmpty = !firstElementId
+    }
 
     return (
         <div
             ref={setNodeRef}
             className={cx(styles.dropZone, {
-                [styles.isOver]: draggingOverFirstPosDropZone,
-                [styles.isEmpty]: !firstElementId,
+                [styles.isOver]: draggingOver,
+                [styles.isEmpty]: isEmpty,
                 [styles.first]: position === FIRST,
                 [styles.last]: position === LAST,
             })}
