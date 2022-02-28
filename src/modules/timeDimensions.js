e6b8f1285abb09dba99a0c93e104f3491865432f
Jen Jones Arnesen <jennifer@dhis2.org>
2022-02-28 12:40:23 +0100

    fix: missed a constant

--- src/modules/timeDimensions.js
+++ src/modules/timeDimensions.js
@@ -76,7 +76,7 @@ export const getEnabledTimeDimensionIds = (inputType, program, stage) => {
         }
 
         if (isEnrollment) {
-            enabledDimensionIds.add(DIMENSION_TYPE_ENROLLMENT_DATE)
+            enabledDimensionIds.add(DIMENSION_ID_ENROLLMENT_DATE)
         }
 
         if (withRegistration) {
